3992cf5e0dc80fc90ee03bca4389119ee55cb470
rustytoe69 <rustytoe5@icloud.com>
2024-04-30 12:55:48 -0400

    Continued working on recycler view for habitDetailsFrag (finished habitDetails class + created recyclerView in layout + began layout file for a single list item (habit_details_list_item_layout))

--- app/src/main/java/com/example/finalproject/HabitDetails.kt
+++ app/src/main/java/com/example/finalproject/HabitDetails.kt
@@ -5,6 +5,4 @@ import android.widget.CheckBox
 //recycler view p1 for HABIT DETAILS FRAG
 data class HabitDetails(
     val date: String,
-//    val isComplete: CheckBox, ????
-//    val courseImageResourceId: Int ????
-)
+    val isChecked: Boolean)
